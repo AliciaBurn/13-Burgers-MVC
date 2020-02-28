9a564235d3f32205c20d42c2fee98e2921fb9356
Alicia Burn <AliciaBurn@gmail.com>
2020-02-28 11:20:24 -0500

    Created connections.js and seeds.sql code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) Values ('The Final Kraut Down Burger
+');
+INSERT INTO burgers (burger_name, devoured) Values ('Sweet Chili O’Mine Burger', true);
+INSERT INTO burgers (burger_name, devoured) Values ('Sweet Dreams Are Made OF Cheese', true);
