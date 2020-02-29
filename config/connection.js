078080ddb0d68da00b720520fa65baba36a073b5
Alicia Burn <AliciaBurn@gmail.com>
2020-02-29 14:26:54 -0500

    Added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 // Set up MYSQL connection
 const mysql = require("mysql");
-
+if(process.env.JAWSDB_URL) {
+    const connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
 const connection = mysql.createConnection({
-    host: "localhost",
+   host: "localhost",
     port: 3306,
     user: "root",
     password: "yourRootPassword",
